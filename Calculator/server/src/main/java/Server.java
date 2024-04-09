ca8662effb896378789b7d8cb5e97fd1d5b69132
NightParker725 <davidleon725@gmail.com>
2024-04-09 12:11:24 -0500

    added host

--- Calculator/server/src/main/java/Server.java
+++ Calculator/server/src/main/java/Server.java
@@ -5,7 +5,7 @@ public class Server
  try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args))
  {
  com.zeroc.Ice.ObjectAdapter adapter = 
- communicator.createObjectAdapterWithEndpoints("SimpleCalculator", "default -p 10000");
+ communicator.createObjectAdapterWithEndpoints("SimpleCalculator", "tcp -h localhost -p 10000");
  com.zeroc.Ice.Object object = new CalculatorI();
  adapter.add(object, com.zeroc.Ice.Util.stringToIdentity("SimpleCalculator"));
  adapter.activate();
